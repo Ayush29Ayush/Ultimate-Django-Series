38a072d2bf0d9b6684a4e87e188b607f75a5cdea
Ayush Senapati <ayushsenapati123@gmail.com>
2024-06-09 00:52:47 +0530

    Added views

--- Ultimate Django Series 1/playground/views.py
+++ Ultimate Django Series 1/playground/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+#! request -> response
+#! request handler
+#! action of the view
+
+
+def say_hello(request):
+    # return HttpResponse("Hello World")
+    context = {"name": "Ayush"}
+    return render(request, "hello.html", context=context)
