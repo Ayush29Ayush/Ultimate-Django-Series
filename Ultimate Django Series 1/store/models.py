6815a4d6312bc4b9faab47103389800e087ac589
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-09 22:42:23 +0530

    Added Many-to-many Relationship

--- Ultimate Django Series 1/store/models.py
+++ Ultimate Django Series 1/store/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    # product_set is be named as products when related_name is set to 'products
+
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
@@ -12,6 +18,8 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+    # promotions = models.ManyToManyField(Promotion, related_name='products')
 
 
 class Customer(models.Model):
