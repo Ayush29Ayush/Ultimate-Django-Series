f91582a135bc27c08fc9f37e89e382a8d81e2139
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-10 08:47:24 +0530

    Added Generic Relations

--- Ultimate Django Series 1/tags/models.py
+++ Ultimate Django Series 1/tags/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    # What tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # What type of object is being tagged (eg. product, article)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # ID
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
