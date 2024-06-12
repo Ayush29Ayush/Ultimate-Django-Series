cf5361776bcc3d23eae2b249738064ab9ab0bb10
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-12 14:26:37 +0530

    Views updated

--- Ultimate Django Series 1/playground/views.py
+++ Ultimate Django Series 1/playground/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product, Customer, Collection, OrderItem
+from store.models import Product, Customer, Collection, OrderItem, Order
 
 # Create your views here.
 #! request -> response
@@ -12,7 +12,10 @@ from store.models import Product, Customer, Collection, OrderItem
 
 def say_hello(request):
 
-    queryset = Product.objects.only("id", "title", "unit_price")
+    # selected_related (1)
+    # prefetch_related (n)
+    # queryset = Product.objects.prefetch_related("promotions").select_related("collection").all()
+    queryset = Order.objects.all().order_by("-placed_at").select_related("customer").prefetch_related("orderitem_set__product")[:5]
 
-    context = {"name": "Ayush", "products": list(queryset)}
+    context = {"name": "Ayush", "orders": list(queryset)}
     return render(request, "hello.html", context=context)
