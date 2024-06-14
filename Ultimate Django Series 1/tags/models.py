734d75abdfb005f6ef9ab14742358a39e6fd119a
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-14 16:31:34 +0530

    Added Model Manager

--- Ultimate Django Series 1/tags/models.py
+++ Ultimate Django Series 1/tags/models.py
@@ -3,12 +3,22 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, model_type, object_id):
+        content_type = ContentType.objects.get_for_model(model_type)
+        return TaggedItem.objects.select_related("tag").filter(
+            content_type=content_type, object_id=object_id
+        )
+
+
 # Create your models here.
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
+    
     # What tag applied to what object
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     # What type of object is being tagged (eg. product, article)
