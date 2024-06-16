3382a44ac2ebc326f7ea90c5c62c133532fb4584
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-16 22:35:27 +0530

    Added serializers.Serializer

---
+++ Ultimate Django Series 2/store/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from decimal import Decimal
+from store.models import Product, Collection
+
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=255)
+
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(
+        max_digits=6, decimal_places=2, source="unit_price"
+    )
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(),
+        view_name="collection-detail",
+    )
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
