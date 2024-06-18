cb12f5f301dad8ef74e54bc834358687585c79d0
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-18 23:07:16 +0530

    Added routers

--- Ultimate Django Series 2/store/urls.py
+++ Ultimate Django Series 2/store/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter, DefaultRouter
 from . import views
+from pprint import pprint
+
+router = SimpleRouter()
+
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
+pprint(router.urls)
 
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
-    path('collections/', views.CollectionList.as_view()),
-]
+# urlpatterns = [
+    # path('products/', views.ProductList.as_view()),
+    # path('products/<int:pk>/', views.ProductDetail.as_view()),
+    # path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
+    # path('collections/', views.CollectionList.as_view()),
+# ]
+
+urlpatterns = router.urls
--- Ultimate Django Series 2/store/views.py
+++ Ultimate Django Series 2/store/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from store.models import Collection, Product
+from store.models import Collection, OrderItem, Product
 from store.serializers import CollectionSerializer, ProductSerializer
 
 
@@ -19,17 +19,26 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {"request": self.request}
     
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response(
                 {
                     "error": "Product cannot be deleted because it is associated with an order item."
-                },
-                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+                }
             )
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+    
+    # def delete(self, request, pk):
+    #     product = get_object_or_404(Product, pk=pk)
+    #     if product.orderitems.count() > 0:
+    #         return Response(
+    #             {
+    #                 "error": "Product cannot be deleted because it is associated with an order item."
+    #             },
+    #             status=status.HTTP_405_METHOD_NOT_ALLOWED,
+    #         )
+    #     product.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CollectionViewSet(ModelViewSet):
@@ -39,16 +48,26 @@ class CollectionViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {"request": self.request}
     
-    def delete(self, request, pk):
-        collection = get_object_or_404(
-            Collection.objects.annotate(products_count=Count("products")).all(), pk=pk
-        )
-        if collection.products_count > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response(
                 {
                     "error": "Collection cannot be deleted because it includes one or more products."
-                },
-                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+                }
             )
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+            
+        return super().destroy(request, *args, **kwargs)
+    
+    # def delete(self, request, pk):
+    #     collection = get_object_or_404(
+    #         Collection.objects.annotate(products_count=Count("products")).all(), pk=pk
+    #     )
+    #     if collection.products_count > 0:
+    #         return Response(
+    #             {
+    #                 "error": "Collection cannot be deleted because it includes one or more products."
+    #             },
+    #             status=status.HTTP_405_METHOD_NOT_ALLOWED,
+    #         )
+    #     collection.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
