f0f568fc1d4ee0ce459eeb76244a3dd786fe62ff
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-19 10:50:05 +0530

    Nested Routers setup completed

--- Ultimate Django Series 2/store/urls.py
+++ Ultimate Django Series 2/store/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter, DefaultRouter
+# from rest_framework.routers import SimpleRouter, DefaultRouter
+from rest_framework_nested import routers
 from . import views
 from pprint import pprint
 
-router = SimpleRouter()
+router = routers.DefaultRouter()
 
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
-pprint(router.urls)
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+urlpatterns = router.urls + products_router.urls
+
+# pprint(router.urls)
+pprint(urlpatterns)
 
 # URLConf
 # urlpatterns = [
@@ -17,5 +24,3 @@ pprint(router.urls)
     # path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
     # path('collections/', views.CollectionList.as_view()),
 # ]
-
-urlpatterns = router.urls
