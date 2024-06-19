1d42468a4972b8e8674af1bcfe64d95a6fe62cf2
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-19 16:09:54 +0530

    Filtering implementation completed for products

--- Ultimate Django Series 2/store/views.py
+++ Ultimate Django Series 2/store/views.py
@@ -10,11 +10,23 @@ from rest_framework.views import APIView
 from rest_framework import status
 from store.models import Collection, OrderItem, Product, Review
 from store.serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from pprint import pprint
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        pprint(self)
+        queryset = Product.objects.all()
+        # collection_id = self.request.query_params['collection_id']
+        collection_id = self.request.query_params.get('collection_id')
+        
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+        
+        return queryset
     
     def get_serializer_context(self):
         return {"request": self.request}
@@ -27,18 +39,6 @@ class ProductViewSet(ModelViewSet):
                 }
             )
         return super().destroy(request, *args, **kwargs)
-    
-    # def delete(self, request, pk):
-    #     product = get_object_or_404(Product, pk=pk)
-    #     if product.orderitems.count() > 0:
-    #         return Response(
-    #             {
-    #                 "error": "Product cannot be deleted because it is associated with an order item."
-    #             },
-    #             status=status.HTTP_405_METHOD_NOT_ALLOWED,
-    #         )
-    #     product.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CollectionViewSet(ModelViewSet):
@@ -57,20 +57,6 @@ class CollectionViewSet(ModelViewSet):
             )
             
         return super().destroy(request, *args, **kwargs)
-    
-    # def delete(self, request, pk):
-    #     collection = get_object_or_404(
-    #         Collection.objects.annotate(products_count=Count("products")).all(), pk=pk
-    #     )
-    #     if collection.products_count > 0:
-    #         return Response(
-    #             {
-    #                 "error": "Collection cannot be deleted because it includes one or more products."
-    #             },
-    #             status=status.HTTP_405_METHOD_NOT_ALLOWED,
-    #         )
-    #     collection.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ReviewViewSet(ModelViewSet):
