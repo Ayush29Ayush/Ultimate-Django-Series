adc45be230ca3db42ba48ce7b8b1f024de6f56fb
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-27 02:12:44 +0530

    feat: CreateOrderSerializer logic updated for OrderItems

--- Ultimate Django Series 2/store/serializers.py
+++ Ultimate Django Series 2/store/serializers.py
@@ -1,20 +1,39 @@
 from rest_framework import serializers
+from django.db import transaction
 from decimal import Decimal
-from store.models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
+from store.models import (
+    Cart,
+    CartItem,
+    Customer,
+    Order,
+    OrderItem,
+    Product,
+    Collection,
+    Review,
+)
 
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ["id", "title", "products_count"]
-        
+
     products_count = serializers.IntegerField(read_only=True)
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["id", "title", "description", "slug", "inventory", "unit_price", "price_with_tax", "collection"]
+        fields = [
+            "id",
+            "title",
+            "description",
+            "slug",
+            "inventory",
+            "unit_price",
+            "price_with_tax",
+            "collection",
+        ]
 
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
 
@@ -27,105 +46,138 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = ["id", "date", "name", "description"]
         # fields = ["id", "date", "name", "description", "product"]
-        
+
     def create(self, validated_data):
         product_id = self.context["product_id"]
         return Review.objects.create(product_id=product_id, **validated_data)
-    
-    
+
+
 class SimpleProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ["id", "title", "unit_price"]
-    
-    
+
+
 class CartItemSerializer(serializers.ModelSerializer):
     product = SimpleProductSerializer()
     total_price = serializers.SerializerMethodField()
-    
+
     def get_total_price(self, cart_item: CartItem):
         return cart_item.quantity * cart_item.product.unit_price
+
     class Meta:
         model = CartItem
         fields = ["id", "product", "quantity", "total_price"]
-        
-        
+
+
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     items = CartItemSerializer(many=True, read_only=True)
     total_price = serializers.SerializerMethodField()
-    
+
     def get_total_price(self, cart: Cart):
-        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+        return sum(
+            [item.quantity * item.product.unit_price for item in cart.items.all()]
+        )
+
     class Meta:
         model = Cart
         fields = ["id", "items", "total_price"]
-        
+
 
 class AddCartItemSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField()
-    
+
     #! This is the convention to validate any particular field i.e validate_{field_name}
     def validate_product_id(self, value):
         if not Product.objects.filter(pk=value).exists():
             raise serializers.ValidationError("No product with the given ID was found")
         return value
-    
+
     def save(self, **kwargs):
         cart_id = self.context["cart_id"]
-        product_id =self.validated_data['product_id']
-        quantity = self.validated_data['quantity']
+        product_id = self.validated_data["product_id"]
+        quantity = self.validated_data["quantity"]
         try:
             cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
             cart_item.quantity += quantity
             cart_item.save()
             self.instance = cart_item
         except CartItem.DoesNotExist:
-            self.instance = cart_item = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
-            
+            self.instance = cart_item = CartItem.objects.create(
+                cart_id=cart_id, **self.validated_data
+            )
+
         return self.instance
-    
+
     class Meta:
         model = CartItem
-        fields = ["id",  "product_id", "quantity"]
-        
-        
+        fields = ["id", "product_id", "quantity"]
+
+
 class UpdateCartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = ["quantity"]
-        
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     #! We want user_id to also be present in the body of the request so declare it here
     user_id = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Customer
         #! Here we need user's id but that is a foreign key relation so we write user_id
         fields = ["id", "user_id", "phone", "birth_date", "membership"]
-        
-        
-class  OrderItemSerializer(serializers.ModelSerializer):
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
     product = SimpleProductSerializer()
+
     class Meta:
         model = OrderItem
         fields = ["id", "product", "unit_price", "quantity"]
-        
+
+
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
+
     class Meta:
         model = Order
         fields = ["id", "customer", "placed_at", "payment_status", "items"]
-        
+
+
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
-    
+
     def save(self, **kwargs):
-        print(self.validated_data['cart_id'])
-        #! Since this is a serializer, we cannot access self.request here, so we will do so and get the user_id from the view itself
-        print(self.context["user_id"])
-        
-        (customer,created) = Customer.objects.get_or_create(user_id=self.context["user_id"])
-        
-        Order.objects.create(customer=customer)
-        
+        with transaction.atomic():
+            print(self.validated_data["cart_id"])
+            #! Since this is a serializer, we cannot access self.request here, so we will do so and get the user_id from the view itself
+            print(self.context["user_id"])
+            
+            cart_id = self.validated_data["cart_id"]
+            # Here get the customer which matches the user_id, or create one if it doesn't exist
+            (customer, created) = Customer.objects.get_or_create(user_id=self.context["user_id"])
+            
+            # Now get the items present in the cart
+            cart_items = CartItem.objects.select_related("product").filter(cart_id=cart_id)
+            
+            # Now for that customer, place the order (DIM Table)
+            order = Order.objects.create(customer=customer)
+            
+            # Get the order items to place in the order from the cart. OrderItem (Fact Table)
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    unit_price=item.product.unit_price,
+                    quantity=item.quantity,
+                )
+                for item in cart_items
+            ]
+            print("order_items", order_items)
+            # Bulk create the order items
+            OrderItem.objects.bulk_create(order_items)
+            # After creating the order, delete the cart
+            Cart.objects.filter(pk=cart_id).delete()
