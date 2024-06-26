8e408539c06137e006541ea861a0161bdfbeb125
Ayush Senapati <ayush.senapati@cozentus.com>
2024-06-27 01:08:14 +0530

    Added a custom serializer for creating order using card_id

--- Ultimate Django Series 2/store/serializers.py
+++ Ultimate Django Series 2/store/serializers.py
@@ -116,3 +116,16 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ["id", "customer", "placed_at", "payment_status", "items"]
+        
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+    
+    def save(self, **kwargs):
+        print(self.validated_data['cart_id'])
+        #! Since this is a serializer, we cannot access self.request here, so we will do so and get the user_id from the view itself
+        print(self.context["user_id"])
+        
+        (customer,created) = Customer.objects.get_or_create(user_id=self.context["user_id"])
+        
+        Order.objects.create(customer=customer)
+        
