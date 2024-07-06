e652793a4e23b9ddfd37afa354afa3b38166acbb
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-06 22:17:52 +0530

    feat: attach files with email

--- Ultimate Django Series 3/playground/views.py
+++ Ultimate Django Series 3/playground/views.py
@@ -1,11 +1,18 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from django.shortcuts import render
 
 
 def say_hello(request):
     try:
-        # send_mail(subject="Django Test Email", message="Hello, this is a test email from django smtp server...", from_email="ayushdjango@gmail.com", recipient_list=["ayushsenapati123@gmail.com"])
-        mail_admins(subject="Django Test Email", message="Hello, this is a test email from django smtp server...", fail_silently=False, html_message="Hello, this is a test email from django smtp server...")
+        message = EmailMessage(
+            subject="Django Test Email",
+            body="Hello, this is a test email from django smtp server...",
+            from_email="ayushdjango@gmail.com",
+            to=["ayushsenapati123@gmail.com"],
+        )
+        message.attach_file("playground/static/images/amazon.png")
+        message.send()
     except BadHeaderError:
         pass
-    return render(request, 'hello.html', {'name': 'Ayush Senapati'})
+
+    return render(request, "hello.html", {"name": "Ayush Senapati"})
