57ca9d926c236d58f49b02db98c566c8bc56f73a
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-11 10:49:34 +0530

    New test cases added

--- Ultimate Django Series 3/store/tests/test_collections.py
+++ Ultimate Django Series 3/store/tests/test_collections.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.test import APIClient
+import pytest
 
+@pytest.mark.django_db
 class TestCreateCollection():
     def test_if_user_is_anonymous_return_401(self):
         #TODO => Format for any test => AAA (Arrange, Act, Assert)
@@ -12,4 +14,10 @@ class TestCreateCollection():
         
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        
+    def test_if_user_is_not_admin_return_403(self):
+        client = APIClient()
+        client.force_authenticate(user = {})
+        response = client.post('/store/collections/', {'title': 'some title'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
         
