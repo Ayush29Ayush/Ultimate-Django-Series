7e4e344ee5192facff1700cf360d6e6610626b7a
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-12 21:13:07 +0530

    JWT token lifetime updated

--- Ultimate Django Series 3/storefront/settings.py
+++ Ultimate Django Series 3/storefront/settings.py
@@ -177,7 +177,7 @@ DJOSER = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=100)
 }
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
