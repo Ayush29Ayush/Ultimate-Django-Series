216ae7d45026cd1d7ef4f73e0810fe1cadac4a7c
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-13 21:47:32 +0530

    feat: TestRetrieveCollection added using model_bakery

--- Ultimate Django Series 3/store/tests/test_collections.py
+++ Ultimate Django Series 3/store/tests/test_collections.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
+from model_bakery import baker
+from store.models import Collection
 
 
 @pytest.fixture
@@ -42,3 +44,19 @@ class TestCreateCollection:
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data["id"] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        #! Arrange
+        collection = baker.make(Collection)
+        #! Act
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
