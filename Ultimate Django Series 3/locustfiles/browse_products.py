c24044a7c1bb31291c320d01c05821c6c31d8ed3
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-17 23:32:10 +0530

    Added new load cases

--- Ultimate Django Series 3/locustfiles/browse_products.py
+++ Ultimate Django Series 3/locustfiles/browse_products.py
@@ -12,19 +12,19 @@ class WebsiteUser(HttpUser):
     #! Higher number means higher priority
     @task(2)
     def view_products(self):
-        print("View products")
+        # print("View products")
         collection_id = randint(2, 6)
         self.client.get(f"/store/products/?collection_id={collection_id}", name="/store/products")
 
     @task(4) 
     def view_product_details(self):
-        print("View product details")
+        # print("View product details")
         product_id = randint(1, 1000)
         self.client.get(f"/store/products/{product_id}", name="/store/products/:id")
 
     @task(1)
     def add_to_cart(self):
-        print("Add to cart")
+        # print("Add to cart")
         product_id = randint(1, 10)
         self.client.post(f"/store/carts/{self.cart_id}/items/", name="/store/carts/items/", json={'product_id': product_id, 'quantity': 1})
 
@@ -33,3 +33,7 @@ class WebsiteUser(HttpUser):
         response = self.client.post("/store/carts/")
         result = response.json()
         self.cart_id = result["id"]
+        
+    @task
+    def say_hello(self):
+        self.client.get("/playground/hello/")
