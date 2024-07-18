bb380fe720736b65d7d51a0bfac47fe9e97ea688
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-18 12:24:17 +0530

    Caching Implementation completed

--- Ultimate Django Series 3/playground/urls.py
+++ Ultimate Django Series 3/playground/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('hello/', views.say_hello)
+    # path('hello/', views.say_hello)
+    path('hello/', views.HelloView.as_view())
+    
 ]
--- Ultimate Django Series 3/playground/views.py
+++ Ultimate Django Series 3/playground/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 import requests
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
 
-def say_hello(request):
-    #! This will get you the response after 2 seconds of request
-    requests.get("http://httpbin.org/delay/2")
-    return render(request, "hello.html", {"name": "Ayush Senapati"})
+# @cache_page(5 * 60)
+# def say_hello(request):
+#     response = requests.get("http://httpbin.org/delay/2")
+#     data = response.json()
+#     return render(request, "hello.html", {"name": data})
+
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60)) #! We cannot utilize decorators directly on class based views so we use method_decorator
+    def get(self, request):
+        response = requests.get("http://httpbin.org/delay/2")
+        data = response.json()
+        return render(request, "hello.html", {"name": "Ayush Senapati"})
