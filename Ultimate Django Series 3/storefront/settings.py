7b10250a1e324538bd6c7f2b67297ee14158bbbd
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-18 12:23:41 +0530

    Added global TIMEOUT for cache

--- Ultimate Django Series 3/storefront/settings.py
+++ Ultimate Django Series 3/storefront/settings.py
@@ -226,6 +226,7 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        },
+        "TIMEOUT": 10 * 60, #! 10 minutes
     }
 }
