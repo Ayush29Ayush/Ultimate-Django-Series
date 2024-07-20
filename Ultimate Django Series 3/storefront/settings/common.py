3898906cd431415b2b2b5ed9524fe91979e60241
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-20 23:03:35 +0530

    feat: Managing Development and Production Settings

--- Ultimate Django Series 3/storefront/settings/common.py
+++ Ultimate Django Series 3/storefront/settings/common.py
@@ -16,19 +16,19 @@ from datetime import timedelta
 from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
+# SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -109,15 +109,15 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront3',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'Ab@123456'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'storefront3',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'Ab@123456'
+#     }
+# }
 
 
 # Password validation
@@ -276,3 +276,5 @@ LOGGING = {
         # },
     }
 }
+
+ 
---
+++ Ultimate Django Series 3/storefront/settings/prod.py
@@ -0,0 +1,8 @@
+from .common import *
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
