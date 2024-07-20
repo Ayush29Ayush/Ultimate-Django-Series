5a6460c827c74b47975b69a78097675380537e3c
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-20 19:44:37 +0530

    Logger setup completed

--- Ultimate Django Series 3/playground/views.py
+++ Ultimate Django Series 3/playground/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 import requests
+import logging
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 
-# @cache_page(5 * 60)
-# def say_hello(request):
-#     response = requests.get("http://httpbin.org/delay/2")
-#     data = response.json()
-#     return render(request, "hello.html", {"name": data})
+# Approach - 1 (Basic)
+# logger = logging.getLogger("playground.views") #! This will make a new logger with name 'playground.views'
+
+# Approach - 2 (Optimized)
+logger = logging.getLogger(
+    __name__
+)  #! This will make a new logger with name 'playground.views' dynamically according to the file name
+
 
 class HelloView(APIView):
-    @method_decorator(cache_page(5 * 60)) #! We cannot utilize decorators directly on class based views so we use method_decorator
     def get(self, request):
-        response = requests.get("http://httpbin.org/delay/2")
-        data = response.json()
+        try:
+            logger.info("Calling httpbin.org/delay/2")
+            response = requests.get("http://httpbin.org/delay/2")
+            logger.info("Received response from httpbin.org/delay/2")
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical("httpbin is offline. Could not connect to httpbin.org/delay/2")
         return render(request, "hello.html", {"name": "Ayush Senapati"})
