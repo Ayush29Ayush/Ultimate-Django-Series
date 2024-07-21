217157d5c8c87c49cea0f19ee9a4edc5ff94d755
Ayush Senapati <ayush.senapati@cozentus.com>
2024-07-21 21:17:08 +0530

    Added docker changes

--- Ultimate Django Series 3/storefront/settings/dev.py
+++ Ultimate Django Series 3/storefront/settings/dev.py
@@ -8,18 +8,22 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'storefront3',
+        'HOST': 'localhost', #! This was used without docker
+        # 'HOST': 'mysql', #! This is used with docker as per the name in the docker-compose.yml 
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': 'Ab@123456'
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://localhost:6379/1' #! This was used without docker
+# CELERY_BROKER_URL = 'redis://redis:6379/1' #! This is used with docker as per the name in the docker-compose.yml
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": "redis://127.0.0.1:6379/2", #! This was used without docker
+        # "LOCATION": "redis://redis:6379/2", #! This is used with docker as per the name in the docker-compose.yml
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
@@ -29,7 +33,12 @@ CACHES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'localhost' #! This was used without docker
+# EMAIL_HOST = 'smtp4dev' #! This is used with docker as per the name in the docker-compose.yml
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
